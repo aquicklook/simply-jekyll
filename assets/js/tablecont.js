9ce1538031059f4797883a811ee3309d9317e11f
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-04 21:43:46 +0700

    Update tablecont.js

--- assets/js/tablecont.js
+++ assets/js/tablecont.js
@@ -1,11 +1,82 @@
+var currentChapter = $('.current-chapter');
+// var prevChapter = currentChapter.closest();
+var nextChapter = currentChapter.next();
 
-$(document).ready(function(){
-  $('#float').on('click',(function(){
-    $('#content').toggleClass("hide show"); 
-    $('#toggle').toggleClass("fa-list-ul fa-times");
-  }));
-  $('#content a').on('click',(function(){//toggle class after a link is clicked
-    $('#content').toggleClass("show hide"); 
-    $('#toggle').toggleClass("fa-times fa-list-ul");
-  }));
+$('.open').on('click', function(e) {
+  e.preventDefault();
+  $('#primary__nav').show().animate({
+    'bottom': '2.95rem'
+  });
+  $('.open').hide();
+  $('.close').show();
 });
+
+$('.close').on('click', function(e) {
+  e.preventDefault();
+  $('#primary__nav').animate({
+    'bottom': '-3rem'
+  });
+  $('.open').show();
+  $('.close').hide();
+});
+
+$('#toc').hide();
+
+$('#table-of-contents').on('click', function() {
+  $('#toc').show().animate({
+    'top': '-78vh'
+  });
+});
+
+$('.toc-link').on('click', function() {
+  var chapterID = $(this).attr('data-tab');
+  $('.chapter').hide();
+  $('#' + chapterID).show();
+  $('.panel').animate({
+    'top': '100%'
+  });
+  $('#primary__nav').animate({
+    'bottom': '-3rem'
+  });
+  $('.open').show();
+  $('.close').hide();
+});
+
+$('.panel-close').on('click', function() {
+  $('.panel').animate({
+    'top': '100%'
+  });
+});
+
+// scrolling controls
+$(window).scroll(function() {
+  // if scroll is greater than 300px, do something
+  if ($(window).scrollTop() > 300) {
+    // attach top bar to the top of the screen
+    $('#top-bar').css({
+      'position' : 'fixed',
+      'top' : '0', 
+      'width' : '100%'
+    });
+    // hides nav menu when scroll is less than 100px
+  } else {
+      $('#top-bar').css({
+        'position' : 'relative'      
+      });
+    } 
+});
+
+
+$('.prev').on('click', function() {
+  currentChapter.prev().show().addClass('current-chapter');
+});
+
+
+$('.next').on('click', function() {
+  // currentChapter.next().addClass('current-chapter');
+  // // $('article').prev().removeClass('current-chapter');
+  nextChapter.show().addClass('current-chapter');
+  currentChapter.hide().removeClass('current-chapter');
+});
+
+
