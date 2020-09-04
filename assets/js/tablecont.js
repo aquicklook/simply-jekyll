5ec3520582612feb1f09049f1eb12a5a66be8159
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-04 22:52:12 +0700

    Update tablecont.js

--- assets/js/tablecont.js
+++ assets/js/tablecont.js
@@ -1,36 +1,49 @@
-(function($){
-
-  var tocbtn = $('li.table-of-contents');
-  var toc    = $('div.table-of-contents');
-
-  function showToC(){
-    tocbtn.addClass('active');
-    toc.hide()
-      .removeClass('hidden')
-      .slideDown(); 
-  }
-
-  function hideToC(){
-    tocbtn.removeClass('active');
-    toc.slideUp(); 
-  }
-
-  tocbtn.on('click', function(){
-    if(!toc.is(':visible'))
-      showToC();
-    else
-      hideToC();
-  });
-
-  $('div.bgimg, div.pages').on('click', function(){
-    if(toc.is(':visible'))
-      hideToC();
-  });
-
-})(jQuery);
-
-(function($){
+$(function(){
   
-  $.fn.hoverEffect = function(){}
+  var volvo = $("#volvo-article");
+  var saab = $("#saab-article");
+  var saabimage = $(".saab-image");
+  var volvoimage= $(".volvo-image");
+  var mercedes = $("#mercedes-article");
+  var image = $(".image-container");
   
-})(jQuery);
+  
+  //mute
+  
+  volvo.hide();
+  mercedes.hide();
+  saab.hide();
+  volvoimage.hide();
+  saabimage.hide();
+  
+$("#contents").change(function(){
+  if($(this).val() =="volvo"){
+    volvo.show();
+    volvoimage.show();
+    saabimage.hide();
+    saab.hide();
+  }
+ else if($(this).val()=="saab"){
+    saab.show();
+    volvo.hide();
+    saabimage.show();
+   volvoimage.hide();
+  }  
+else if($(this).val()=="showall"){
+  volvo.show();
+  saab.show();
+}
+  else if ($(this).val() == "allimages"){
+    volvo.hide();
+    saab.hide();
+    volvoimage.show();
+    saabimage.show();
+  }
+  else{
+    volvo.hide();
+    saab.hide();  volvoimage.hide();
+  saabimage.hide();
+  }
+})
+  
+});
