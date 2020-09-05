e941d100e17a6952a82b5ddad48b1f75811878d5
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-06 06:35:31 +0700

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,20 +1,8 @@
-function toggle_visibility(toc) {
-var f = document.getElementById(toc);
-if(f.style.display == 'none')
-  f.style.display = 'block';
-else
-f.style.display = 'none';
-$('.Hide').click(function() {
-    $('#target').hide(500);
-    $('.Show').show(0);
-    $('.Hide').hide(0);
-});
-   $('.Show').click(function() {
-    $('#target').show(500);
-    $('.Show').hide(0);
-    $('.Hide').show(0);
-});
-$('.toggle').click(function() {
-    $('#target').toggle('slow');
-});
-    }
+function tocContainer() {
+  var x = document.getElementById("toc-container");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
