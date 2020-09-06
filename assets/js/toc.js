3c9878cc1d196ff6d295ad220971445b4775ad70
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-06 11:24:57 +0700

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -6,3 +6,15 @@ function tocContainer() {
     x.style.display = "none";
   }
 }
+
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){  
+   var st = window.pageYOffset || document.documentElement.scrollTop;  
+   if (st > lastScrollTop){
+       document.getElementById("bottommenu").style.bottom = "-100%";
+   } else {
+      document.getElementById("bottommenu").style.bottom = "0";
+   }
+   lastScrollTop = st;
+}, false);
