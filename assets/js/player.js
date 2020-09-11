337b67e63aee70028584f550a4e02275e8f66795
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 16:39:34 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = new Plyr('#player', { controls , settings, i18n });
   window.player = player;
   player.volume = 0.5;
-  player.speed = { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] };
+  player.speed = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
