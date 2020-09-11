3ec1958b08e4dc220cbc57965554a06bdb6c6d6b
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 20:40:10 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,40 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const controls = ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'airplay', 'fullscreen']
   const settings = ['speed']
   const i18n = { speed: 'Speed' }
+  const keyboard = { global: true }
+  const tooltips = { controls: true }
+  const captions = { active: true }
+  const previewThumbnails = { enabled: true , src: ['https://cdn.plyr.io/static/demo/thumbs/100p.vtt'] }
+  const vimeo = { referrerPolicy: 'no-referrer' }
+  const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container' }
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { 
-     debug: true,
-  title: 'View From A Blue Moon',
-  iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg',
-  keyboard: {
-    global: true,
-  },
-  tooltips: {
-    controls: true,
-  },
-  captions: {
-    active: true,
-  },
-  previewThumbnails: {
-    enabled: true,
-    src: [
-      'https://cdn.plyr.io/static/demo/thumbs/100p.vtt',
-    ],
-  },
-  vimeo: {
-    referrerPolicy: 'no-referrer',
-  },
-  fullscreen: {
-    enabled: true,
-    fallback: true,
-    iosNative: false,
-    container: '#container',
-  }, 
-    controls , 
-    settings , 
-    i18n , 
-    speed 
-  });
+  const player = new Plyr('#player', { debug: true , title: 'Quicklook Play Video' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , settings , i18n , speed , keyboard , tooltips , captions , previewThumbnails , vimeo , fullscreen });
   window.player = player;
   player.volume = 0.5;
   function on(selector, type, callback) {
