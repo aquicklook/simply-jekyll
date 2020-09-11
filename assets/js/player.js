58f172c963295c5b56c5e0babb20d222d3b688a2
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 19:08:48 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const defaults = { seekTime: 5 }
   const i18n = { speed: 'Playback speed' }
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { controls , settings, i18n , speed });
+  const player = new Plyr('#player', { controls , settings, i18n , speed, defaults });
   window.player = player;
   player.volume = 0.5;
   function on(selector, type, callback) {
