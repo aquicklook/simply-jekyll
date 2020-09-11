4ecd98cd5dbd00fb06bd32aaec9f072108733d81
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 22:42:52 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => { 
-  const controls = ['play-large', 'play', 'progress', 'current-time', 'mute', 'captions', 'settings', 'airplay', 'fullscreen']
+  const controls = ['play-large', 'play', 'progress', 'current-time', 'volume', 'mute', 'captions', 'settings', 'airplay', 'fullscreen']
   const settings = ['speed']
   const i18n = { speed: 'Speed' }
   const keyboard = { global: true }
