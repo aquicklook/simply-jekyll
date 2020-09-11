d4c92f00a00a6be646d0b6044d65418e19e85b2b
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 09:10:51 +0700

    Create player.js

---
+++ assets/js/player.js
@@ -0,0 +1,7 @@
+	document.addEventListener('DOMContentLoaded', () => { 
+		const player = new Plyr('#player');
+		window.player = player;
+		function on(selector, type, callback) 
+		{document.querySelector(selector).addEventListener(type, callback, false);
+		}
+		});
