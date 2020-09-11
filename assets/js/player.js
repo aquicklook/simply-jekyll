a7a8412de8689c9881bc7ad1da564143b917ab7d
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 12:17:04 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = new Plyr('#player');
   window.player = player;
   player.volume = 0.5;
-  controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'];
+  controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'airplay', 'fullscreen'];
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
