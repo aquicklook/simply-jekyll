00edc7b3bcd0bcbca0172d56d089513fb5cb85bd
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 22:10:04 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -6,10 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const tooltips = { controls: true }
   const captions = { active: true }
   const vimeo = { referrerPolicy: 'no-referrer' }
-  const options = { controlBar: { volumePanel: { inline: false , vertical: true }}}
   const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container' }
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { debug: true , title: 'Quicklook Play Video' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , settings , i18n , speed , keyboard , tooltips , captions , vimeo , options , fullscreen });
+  const player = new Plyr('#player', { debug: true , title: 'Quicklook Play Video' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , settings , i18n , speed , keyboard , tooltips , captions , vimeo , fullscreen });
   window.player = player;
   player.volume = 0.5;
   function on(selector, type, callback) {
