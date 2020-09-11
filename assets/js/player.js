7b4ee8ab34acd055c67bb33c57849a8fa4fc8506
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 12:16:36 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,23 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = new Plyr('#player');
   window.player = player;
   player.volume = 0.5;
+  controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'];
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
-  controls: [
-    'play-large', // The large play button in the center
-    'restart', // Restart playback
-    'rewind', // Rewind by the seek time (default 10 seconds)
-    'play', // Play/pause playback
-    'fast-forward', // Fast forward by the seek time (default 10 seconds)
-    'progress', // The progress bar and scrubber for playback and buffering
-    'current-time', // The current time of playback
-    'duration', // The full duration of the media
-    'mute', // Toggle mute
-    'volume', // Volume control
-    'captions', // Toggle captions
-    'settings', // Settings menu
-    'airplay', // Airplay (currently Safari only)
-    'fullscreen', // Toggle fullscreen
-  ];
 });
