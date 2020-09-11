fce86d854306db4023656c400c58baa3af3867dc
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 11:43:56 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => { 
   const player = new Plyr('#player');
   window.player = player;
+  player.volume = 0.75;
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
