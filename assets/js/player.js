d60519ee4aed247d15b74c8028758b2bee5118b1
Quicklook <68094278+aquicklook@users.noreply.github.com>
2020-09-11 13:01:16 +0700

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = new Plyr('#player', { controls });
   window.player = player;
   player.volume = 0.5;
-  player.pip = disable;
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
